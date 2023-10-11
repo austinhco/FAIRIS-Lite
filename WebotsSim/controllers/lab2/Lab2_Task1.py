ac1af8362e18c8de0c391e8b3c374d06a4d05ef4
austinhco <seven.pin.lightning@gmail.com>
2023-10-11 16:30:46 -0400

    finished lab 2?

--- WebotsSim/controllers/lab2/Lab2_Task1.py
+++ WebotsSim/controllers/lab2/Lab2_Task1.py
@@ -15,7 +15,7 @@ emoo = Emoo()
 
 # reset robot position
 # Loads the environment from the maze file
-maze_file = 'worlds/mazes/Labs/Lab2/Lab2_1.xml'
+maze_file = 'worlds/mazes/Labs/Lab2/Lab2_2.xml'
 emoo.load_environment(maze_file)
 emoo.move_to_start()
 
@@ -27,26 +27,25 @@ emoo.initial_fre = emoo.get_front_right_motor_encoder_reading()
 emoo.speed_pref = 20
 emoo.angular_speed_pref = 20
 emoo.rotational_speed_pref = 2
-emoo.linear_precision_pref = 0.01
-emoo.angular_precision_pref = 1
+emoo.linear_precision_pref = 0.005
+emoo.angular_precision_pref = 3
 emoo.wall_error_precision_pref = 0.05
 emoo.braking_distance = 0.5
 emoo.braking_velocity = 10
 emoo.angular_braking_velocity = 10
-emoo.wall_avoidance_aggression = 1.2
+emoo.wall_following_speed = 10
 
 # Set sensor parameters
-emoo.range_width = 5
+emoo.range_width = 10
 
 # Set PID parameters
-emoo.pid_k = 1  # kp
+emoo.pid_kp = 1  # k forward
+emoo.pid_ks = 4  # k side
 
 # Set pose estimate
 emoo.estimated_x = 0
 emoo.estimated_y = -1.5
 
-emoo.move_arc_angle(10, 0.5)
-emoo.move_linear(0.1)
 while 1:
-    emoo.move_through(0.5, 0.3)
+    emoo.follow_wall("right", 0.3)
     emoo.advance()
